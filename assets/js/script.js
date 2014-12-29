97fe244018a9c4d827c999f780b10d0ca15d1733
AlicJ <zichen.jiang@yahoo.com>
2014-12-29 16:20:24 -0500

    fix ajax load page problem

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,22 @@
-
+var page = "";
 
 $(document).on('click', '.navAjax', function (event) {
 	event.preventDefault();
-	console.log(event);
-	window.location.replace($(this).attr('href'));
+	page = $(this).attr('href').replace("/","");
+	loadPage(page);
 });
 
 $(document).ready(function() {
-	var args = Arg.all();
-	url = {queryKey:  args};
-	var a = $('<a>', { href:document.URL } )[0];
-	var path = a.pathname.charAt(0) == '/' ? a.pathname.substring(1) : a.pathname;
-
-	console.log(path);
-
-	switch(path){
-		case "":
-			$("#ajax-content").load('assets/pages/index.html');
-		default:
-			$("#ajax-content").load('assets/pages/'+path+'.html');
-	}
+	loadPage(page);
 
 });
+
+function loadPage (page) {
+	switch(page){
+		case "":
+			$("#ajax-content").load('assets/pages/index.html');
+			break;
+		default:
+			$("#ajax-content").load('assets/pages/'+page+'.html');
+	}
+}
