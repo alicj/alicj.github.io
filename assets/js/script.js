40f78cb69d726e0e78eebe598a105e0f6653a9e0
AlicJ <zichen.jiang@yahoo.com>
2014-12-29 16:07:02 -0500

    set up basic nav skeleton

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,23 @@
 
 
+$(document).on('click', '.navAjax', function (event) {
+	event.preventDefault();
+	window.location.replace($(this).attr('href'));
+});
+
 $(document).ready(function() {
 	var args = Arg.all();
 	url = {queryKey:  args};
 	var a = $('<a>', { href:document.URL } )[0];
 	var path = a.pathname.charAt(0) == '/' ? a.pathname.substring(1) : a.pathname;
 
-	console.log(path)
+	console.log(path);
+
+	switch(path){
+		case "":
+			$("#ajax-content").load('assets/pages/index.html');
+		default:
+			$("#ajax-content").load('assets/pages/'+path+'.html');
+	}
+
 });
