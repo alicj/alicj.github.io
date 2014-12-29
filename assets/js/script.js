ec988a3a6b55df9bdb91a61b44c1e733e618b03d
AlicJ <zichen.jiang@yahoo.com>
2014-12-29 16:39:11 -0500

    finish navigation

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,20 +3,36 @@ var page = "";
 $(document).on('click', '.navAjax', function (event) {
 	event.preventDefault();
 	page = $(this).attr('href').replace("/","");
-	loadPage(page);
+	setPage(page)
+	loadPage();
 });
 
 $(document).ready(function() {
-	loadPage(page);
-
+	loadPage();
 });
 
-function loadPage (page) {
-	switch(page){
+function setPage(curPage){
+	localStorage.setItem('lastViewdPage', curPage)
+}
+
+function loadPage() {
+	var isValid = localStorage.getItem('lastViewdPage');
+	var curPage = isValid ? isValid : "";
+
+	$("#ajax-content").hide();
+	switch(curPage){
 		case "":
 			$("#ajax-content").load('assets/pages/index.html');
 			break;
 		default:
-			$("#ajax-content").load('assets/pages/'+page+'.html');
+			$("#ajax-content").load('assets/pages/'+curPage+'.html');
 	}
+	setNavActive(curPage);
+	$("#ajax-content").fadeIn('fast');
+
+}
+
+function setNavActive(curPage){
+	$('.navAjax').removeClass('current');
+	$('.navAjax[href="/'+curPage+'"]').addClass('current');
 }
