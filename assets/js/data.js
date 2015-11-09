e2d53f21e08312bca84bd5471fdfce156a405466
alicj <zichen.jiang@yahoo.com>
2015-11-08 23:00:35 -0500

    add new font icons

--- assets/js/data.js
+++ assets/js/data.js
@@ -2,16 +2,19 @@
 
 PROJECTS = [
 	{
+		title: "CentraCare",
+		note: "DementiaHacks 2015, Toronto",
+		type: "internet",
+		content: "Working with another programmer and three other health science students, we built a prototype for a platform for caregivers to have better understanding of the patients with dementia. Due to the nature of the diesease, the patients tend to avoid social interaction, which is actually necessary for them and would help them stay healthier. So we built this platform for the families of patients to fill out for the caregivers to read prior to their arrival to help build a connection between them and the patient, that they would understand what actions to take under certain circumstances, and have meaningful conversations and realationships with the patiens.",
+		date: "Nov 7-8, 2015",
+		link: "https://github.com/Jimicy/CentraCare"
+
+	},
+	{
 		title: "Gloss Gallery",
 		note: false,
 		type: "internet",
-		content: "\
-			A gallery made upon professor's request to hold the top animations \
-			written in Haskell submitted from class CS1JC3.\
-			Some responsive design ideas are applied. New layout updated in the \
-			start of Janurary, 2015. Github repository can be found \
-			<a href='https://github.com/AlicJ/GlossGallery'>here</a>.\
-		",
+		content: "A gallery made upon professor's request to hold the top animations written in Haskell submitted from class CS1JC3. Some responsive design ideas are applied. New layout was updated in the start of Janurary, 2015. Github repository can be found <a href='https://github.com/AlicJ/GlossGallery'>here</a>.",
 		date: "Jan 2015/ May 2014",
 		link: "GlossGallery/index.html"
 	},
@@ -19,14 +22,7 @@ PROJECTS = [
 		title: "AgentFlux",
 		note: "Web Developer Co-op @ AgentFlux Inc.",
 		type: "computer",
-		content: "\
-			I worked in a team of seven people to create a web application \
-			with JavaScript and HTML.\
-			I also worked with Zend Framework to create certain APIs.\
-			AgentFlux is an application across all platforms that provides \
-			real estate agents a revolutionary way to connect with their clients\
-			 - share properties and schedule appointments with them.\
-		",
+		content: "I worked in a team of seven people to create a web application with JavaScript and HTML. I also worked with Zend Framework to create certain APIs. AgentFlux is an application across all platforms that provides  real estate agents a revolutionary way to connect with their clients - share properties and schedule appointments with them.",
 		date: "May - Aug 2014",
 		link: "http://agentflux.com/"
 	},
@@ -34,12 +30,7 @@ PROJECTS = [
 		title: "Vshore Website",
 		note: false,
 		type: "computer",
-		content: "A website made for Vshore Inc. I used JavaScript to build \
-		several functions to reduce workload, like writting the header and the footer\
-		automatically in every page. A screen capture of the site can be found \
-		<a target='_blank' href='assets/img/vshore.png'>here</a> (the site is no longer live). An upgrade demo of the site \
-		can be found <a target='_blank' href='vshore-demo/index.html'>here</a>, yet it was not implemented due to client's company issue. \
-		",
+		content: "A website made for Vshore Inc. I used JavaScript to build several functions to reduce workload, like writting the header and the footer automatically in every page. A screen capture of the site can be found  <a target='_blank' href='assets/img/vshore.png'>here</a> (the site is no longer live). An upgrade demo of the site can be found <a target='_blank' href='vshore-demo/index.html'>here</a>, yet it was not implemented due to client's company issue.",
 		date: "Oct 2013",
 		link: "vshore-demo/index.html"
 	},
@@ -47,15 +38,7 @@ PROJECTS = [
 		title: "Cyber Blaster",
 		note: "TechU.me Program",
 		type: "mobile",
-		content: "A program in which I worked with a high shcool peer, and a group of Grade Three students who act\
-		as clients. We made an educational web-based app based on their proposal.\
-		This is the first Web App I ever wrote. We learnt JavaScript, HTML, and \
-		<a href='http://en.wikipedia.org/wiki/Systems_development_life_cycle'>SDLC</a> in a month during class independently.\
-		The entire development is docummented on a weekly basis, as we applied some Agile and Lean ideas.\
-		Checkout the Github repo <a href='https://github.com/AlicJ/CyberBlaster/'>here</a>.\
-		Also we lunched the app onto a BlackBerry Playbook using \
-		<a href='http://developer.blackberry.com/html5/'>WebWorks</a>.\
-		",
+		content: "A program in which I worked with a high shcool peer, and a group of Grade Three students who actas clients. We made an educational web-based app based on their proposal.This is the first Web App I ever wrote. We learnt JavaScript, HTML, and <a href='http://en.wikipedia.org/wiki/Systems_development_life_cycle'>SDLC</a> in a month during class independently.The entire development is docummented on a weekly basis, as we applied some Agile and Lean ideas. Checkout the Github repo <a href='https://github.com/AlicJ/CyberBlaster/'>here</a>. Also we lunched the app onto a BlackBerry Playbook using <a href='http://developer.blackberry.com/html5/'>WebWorks</a>.",
 		date: "Jan - June 2013",
 		link: "CyberBlaster/index.html"
 	}
